bcec2496f2736db6f9a983a6dd89998d442783d7
miniyzf <mini.yzf@qq.com>
2019-11-11 18:18:17 +0800

    netty test

--- src/main/java/com/example/netty/common/Common.java
+++ src/main/java/com/example/netty/common/Common.java
@@ -4,5 +4,5 @@ public class Common {
     //  WebSocket端口号
     public static final int WEB_SOCKET_PORT = 1234;
     //  WebSocket地址
-    public static final String WEB_SOCKET_URL="ws://localhost:"+WEB_SOCKET_PORT+"/ws";
+    public static final String WEB_SOCKET_URL="ws://localhost:"+WEB_SOCKET_PORT;
 }
